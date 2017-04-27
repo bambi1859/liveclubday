d7f4766df8dbe5ac67f4bd24e791e52b6bc73f61
yein kim <hoyein9297@naver.com>
2017-04-27 02:35:26 +0000

    first

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,29 @@
+class HomeController < ApplicationController
+    
+    def main
+    end
+    
+    def about1
+    end
+    
+    def about2
+    end
+    
+    def program1
+    end
+    
+    def program2
+    end
+    
+    def supporters1
+    end
+    
+    def supporters2
+    end
+    
+    def supporters3
+    end
+    
+    def notice
+    end
+end
